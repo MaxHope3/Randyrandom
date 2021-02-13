187397df595c35550ee581aedfe213bd6cf0af72
Maximilian Hoppe <mhoppe1996@gmail.com>
2021-02-13 19:43:40 +0100

    play selected playlist on shuffle

--- randy.py
+++ randy.py
@@ -2,6 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
 import os.path
+import time
 
 redirectUri = "https://example.com/callback"
 fileName = "secrets.json"
@@ -22,7 +23,7 @@ else:
 #print(clientSecret)
 #print(redirectUri)
 
-scope = "user-library-read"
+scope = "user-library-read streaming user-read-playback-state"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=clientId, client_secret=clientSecret, redirect_uri=redirectUri))
 
@@ -39,7 +40,17 @@ print("playing " + playlistName)
 for playlist in playlists['items']:
     if playlist['name'] == playlistName:
         playlistId = playlist['id']
+        playlistUri = playlist['uri']
         break
 # TODO: errorhandling if playlist not found
 
-print(playlistId)
+sp.start_playback(context_uri=playlistUri)
+sp.shuffle(True)
+
+# continuously read
+# break with Ctr+C
+while(True):
+    currentPlayback = sp.current_playback()
+    print(currentPlayback)
+    time.sleep(10)
+    sp.next_track()
