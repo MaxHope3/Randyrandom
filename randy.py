ce8850aa9efb018a7cce64f8a54b0cf64d08ee61
Maximilian Hoppe <mhoppe1996@gmail.com>
2021-02-13 17:51:38 +0100

    add login

--- randy.py
+++ randy.py
@@ -1,9 +1,19 @@
-import random
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 clientId = input("enter client id: ")
 clientSecret = input("enter user secret: ")
-redirectUri = input("ender rediret uri: ")
+redirectUri = "https://example.com/callback"
 
-print(clientId)
-print(clientSecret)
-print(redirectUri)
+#print(clientId)
+#print(clientSecret)
+#print(redirectUri)
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=clientId, client_secret=clientSecret, redirect_uri=redirectUri))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
