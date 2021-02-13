39962377309d81863e92ed5294cd616de00f8b7e
Maximilian Hoppe <mhoppe1996@gmail.com>
2021-02-13 18:25:52 +0100

    cache auth locally

--- randy.py
+++ randy.py
@@ -1,9 +1,24 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import json
+import os.path
 
-clientId = input("enter client id: ")
-clientSecret = input("enter user secret: ")
+clientId = ""
+clientSecret = ""
 redirectUri = "https://example.com/callback"
+fileName = "secrets.json"
+
+if os.path.isfile(fileName):
+    with open(fileName) as openedFile:
+        data = json.load(openedFile)
+        clientId = data['name']
+        clientSecret = data['secret']
+else:
+    clientId = input("enter client id: ")
+    clientSecret = input("enter user secret: ")
+    secrets = {"name":clientId, "secret":clientSecret}
+    with open(fileName, "w") as json_file:
+        json.dump(secrets, json_file)
 
 #print(clientId)
 #print(clientSecret)
@@ -13,7 +28,11 @@ scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=clientId, client_secret=clientSecret, redirect_uri=redirectUri))
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+playlists = sp.current_user_playlists()
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
