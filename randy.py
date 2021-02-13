8c4e29c52bcc0243d781a563bc516c03de0659f0
Maximilian Hoppe <mhoppe1996@gmail.com>
2021-02-13 17:38:57 +0100

    added input auth

--- randy.py
+++ randy.py
@@ -1,1 +1,9 @@
 import random
+
+clientId = input("enter client id: ")
+clientSecret = input("enter user secret: ")
+redirectUri = input("ender rediret uri: ")
+
+print(clientId)
+print(clientSecret)
+print(redirectUri)
